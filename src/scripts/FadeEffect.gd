472055b4312834260c9f0d285162144a10b31fb6
Jason <beefers331@gmail.com>
2020-04-12 02:00:54 -0600

    Implemented fade

---
+++ src/scripts/FadeEffect.gd
@@ -0,0 +1,30 @@
+extends ViewportContainer
+
+var timeForFade : float = 1
+var timeRemaining : float = 1
+var viewPort : ViewportContainer
+var firedOnFaded : bool = false
+var fadeIn : bool = false
+
+func _ready():
+	_fade(2,true,null)
+
+func _fade(time : float,doFadeIn: bool , tex:Texture):
+	if(tex != null):
+		material.set_shader_param("animTexure",tex)
+	
+	timeForFade = time
+	timeRemaining = time
+	firedOnFaded = false
+
+	if(doFadeIn):
+		self.fadeIn = doFadeIn
+		timeRemaining = 0
+
+func _process(_delta):
+	if(fadeIn): timeRemaining += _delta
+	else: timeRemaining -= _delta
+	material.set_shader_param("progress",timeRemaining/timeForFade)
+	#Call on complete event here
+	if((timeRemaining <= 0 and !firedOnFaded and !fadeIn) || (timeRemaining >= timeForFade and !firedOnFaded and fadeIn)):pass
+
---
+++ src/vfx/shaders/screenspace/fade.shader
@@ -0,0 +1,13 @@
+shader_type canvas_item;
+
+uniform sampler2D animTexture : hint_black_albedo;
+uniform float progress :  hint_range(0, 1);
+
+void fragment() {
+    vec3 col = texture(TEXTURE, SCREEN_UV).xyz;
+	vec2 tSize = 1.0/vec2(textureSize(animTexture,0));
+	vec2 coord = round(SCREEN_UV/tSize) * tSize;
+	if(texture(animTexture,coord).r > progress) col = vec3(0.0);
+
+    COLOR.xyz = col;
+}
