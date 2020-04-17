d895a8e9d70d5774f0bd23b260419be75df3bbf6
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-04-17 19:57:27 -0400

    Rework AnimatedAtlas to stay in sync

--- src/scripts/AnimatedAtlas.gd
+++ src/scripts/AnimatedAtlas.gd
@@ -7,23 +7,14 @@ export(Array, Texture) var frames = []
 export var frames_per_second: float = 5
 export var repeat_delay: float = 0
 
-var t = 0
-var rt = 0
-
-func _process(delta):
-	if rt > 0:
-		rt -= delta
-		return
-
-	t += delta
+func _process(_delta):
+	var t = OS.get_ticks_msec() / 1000.0
 	
 	var frame_time = 1.0 / frames_per_second
 	var cycle_time = frame_time * frames.size()
-	var frame = int(t / frame_time)
+	var ct = fmod(t, cycle_time + repeat_delay)
+	
+	var frame = 0 if ct >= cycle_time else int(ct / frame_time)
 	
 	if texture.atlas:
-		texture.atlas = frames[frame % frames.size()]
-		
-	if t > cycle_time:
-		rt = repeat_delay
-		t -= cycle_time
+		texture.atlas = frames[frame]
