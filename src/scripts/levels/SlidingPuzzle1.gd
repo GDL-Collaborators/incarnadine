332679140d3bfc7eb2d09e4fc4546ce5dc260673
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-04-21 22:07:26 -0400

    Don't unlock sliding puzzle by default

--- src/scripts/levels/SlidingPuzzle1.gd
+++ src/scripts/levels/SlidingPuzzle1.gd
@@ -3,10 +3,14 @@ extends Node2D
 var test: Node2D
 
 func _ready():
-	$'/root/GameState'.unlock_exit()
 	$'YSort/Step Button'.connect('pressed', self, 'reset_puzzle')
+	$UnlockArea.connect('body_entered', self, 'maybe_unlock')
 
 func reset_puzzle():
 	for obj in $YSort.get_children():
 		if obj.name.begins_with('PushBlock'):
 			obj.reset()
+
+func maybe_unlock(body):
+	if body.is_in_group('player'):
+		GameState.unlock_exit()
