9fd59b8f68aa2ba8034fe4feaf75d7c1ef54591a
jon <jonathanharvey22@gmail.com>
2020-04-27 01:10:43 +0200

    added missing file from previous commit
    cairns.gd was missing.

---
+++ src/scripts/levels/Cairns.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+
+var _solution_array = ['isaz', 'sowilo', 'mannaz', 'hagalaz', 'berkanan']
+
+var _player_array = []
+
+func _on_CairnMannaz_mannaz():
+	_player_array.push_back('mannaz')
+	_test()
+	_check_if_done()
+
+func _on_CairnHagalaz_hagalaz():
+	_player_array.push_back('hagalaz')
+	_test()
+	_check_if_done()
+
+func _on_CairnIsaz_isaz():
+	_player_array.push_back('isaz')
+	_test()
+	_check_if_done()
+
+func _on_CairnSowilo_sowilo():
+	_player_array.push_back('sowilo')
+	_test()
+	_check_if_done()
+
+func _on_CairnBerkanan_berkanan():
+	_player_array.push_back('berkanan')
+	_test()
+	_check_if_done()
+	
+func _check_if_done():
+	if _player_array.size() == _solution_array.size():
+		_check_solution()
+
+func _check_solution():
+	if _player_array == _solution_array:
+		GameState.unlock_exit()
+	elif _player_array != _solution_array:
+		_player_array.empty()
+
+func _test():
+	if _player_array.size() >= 0 :
+		print('_player_array')
