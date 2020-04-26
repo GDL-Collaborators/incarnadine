17c743942317ce479e0c76175c574e7794108b30
jon <jonathanharvey22@gmail.com>
2020-04-27 00:27:39 +0200

    Cairns level added
    no changes to anything else, just added resources for the scene and the scene itself.

---
+++ src/scripts/CairnHagalaz.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+signal hagalaz
+
+func _interact(_player):
+	emit_signal("hagalaz")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
