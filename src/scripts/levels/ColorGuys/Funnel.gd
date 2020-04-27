65ecb3be98d9340dbf274fa51b327c817be3b949
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-04-27 01:32:12 -0400

    Funnel sound

--- src/scripts/levels/ColorGuys/Funnel.gd
+++ src/scripts/levels/ColorGuys/Funnel.gd
@@ -1,12 +1,17 @@
 extends StaticBody2D
 
+var splat_sound = preload('res://assets/sfx/squish.wav')
+
 func _interact(player):
 	if player.has_item('paintr'):
 		player.rem_item('paintr')
+		Audio.play_sfx(splat_sound)
 		owner.get_node('FloodPuzzle').fill_color('red')
 	if player.has_item('paintg'):
 		player.rem_item('paintg')
+		Audio.play_sfx(splat_sound)
 		owner.get_node('FloodPuzzle').fill_color('green')
 	if player.has_item('paintb'):
 		player.rem_item('paintb')
+		Audio.play_sfx(splat_sound)
 		owner.get_node('FloodPuzzle').fill_color('blue')
